006af247890a1d6d4ebd21e1529acf8f25a67b5e
Rejey <rehxxz@gmail.com>
2023-11-04 18:09:14 +0800

    Did some hcanges and added vite and react

---
+++ Memory_Sports_React/src/App.jsx
@@ -0,0 +1,20 @@
+import styles from './style'
+
+const App = () => {
+  return (
+    <div className="bg-primary w-full overflow-hidden">
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          Navbar
+        </div>
+      </div>
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          Hero Main
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
